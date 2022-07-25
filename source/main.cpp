b5ac314ba6ee2abdecf2c1f2d5e108c3b6c3504d
Hendrik Lehmann <hendrik.lehmann96@gmail.com>
2022-07-25 08:33:22 +0200

    simple input handling

--- source/main.cpp
+++ source/main.cpp
@@ -2,8 +2,16 @@
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
 	(void)argv;
-	std::cout << "Hello World!" << std::endl;
+	try
+	{
+		if (argc != 3)
+			throw std::runtime_error("Please execute the binary as follows: ./ircserv <port> <password>");
+	}
+	catch (const std::exception &exception)
+	{
+		std::cerr << exception.what() << std::endl;
+		return (1);
+	}
 	return (0);
 }
