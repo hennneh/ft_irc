19f6b709192463000f8b7c12fb646e7840873cfc
Peter Stengl <pstengl@student.42wolfsburg.de>
2022-07-25 17:54:43 +0200

    Add crude implementation for Welcome messages To be set as online in Adium (the current Client)

--- source/irc.cpp
+++ source/irc.cpp
@@ -2,6 +2,12 @@
 #include <string>
 #include <sstream>
 
+void ft_sendmsg(int socketfd, std::string msg) {
+	msg = ":127.0.0.1 " + msg;
+	send(socketfd, (msg + "\r\n").c_str(), msg.length() + 2, 0);
+	std::cout << "Sending: '" << msg << "\\r\\n'" << std::endl;
+}
+
 // Constructor
 ft::IRC::IRC(const int& port, const std::string& password): _port(port), _password(password) {
 	struct protoent		*prtdb;
@@ -79,19 +85,39 @@ void	ft::IRC::run() {
 						msg.erase(std::remove(msg.begin(), msg.end(), '\n'), msg.end());
 						if (msg.length() == 0)
 							continue;
-						if (msg == "QUIT") {
+						std::cout << TXT_FAT << "Client " << i << ": " << TXT_NUL << msg << std::endl;
+						if (msg.compare(0, 4, "NICK") == 0) {
+							std::string username(msg.substr(5, msg.length() - 5));
+							ft_sendmsg(this->_connections[i], "001 " + username + " :Welcome to the Internet Relay Network " + username + "!*@127.0.0.1");
+							ft_sendmsg(this->_connections[i], "002 " + username + " :Your host is 127.0.0.1, running version ft_irc0.1");
+							ft_sendmsg(this->_connections[i], "003 " + username + " :This server was created 2022-07-25");
+							ft_sendmsg(this->_connections[i], "004 " + username + " 127.0.0.1 ft_irc0.1 iswo opsitnmlbvk");
+							ft_sendmsg(this->_connections[i], "375 " + username + " :-- Message of the day --");
+							ft_sendmsg(this->_connections[i], "372 " + username + " :-    Hello People!     -");
+							ft_sendmsg(this->_connections[i], "376 " + username + " :-- Message of the day --");
+							continue;
+						}
+						if (msg.compare(0, 4, "USER") == 0)
+							continue;
+						if (msg.compare(0, 4, "PING") == 0) {
+							std::string token(msg.substr(5, msg.length() - 5));
+							ft_sendmsg(this->_connections[i], "PONG " + token);
+							continue;
+						}
+						if (msg.compare(0, 4, "PONG") == 0)
+							continue;
+						if (msg.compare(0, 4, "QUIT") == 0) {
 							send(this->_connections[i], "bye!\n", 5, 0);
 							close(this->_connections[i]);
 							std::cout << TXT_FAT << "Client " << i << " disconnected!" << TXT_NUL << std::endl;
 							this->_connections.erase(this->_connections.begin() + i);
 							break;
 						}
-						if (msg == "SHUTDOWN") {
+						if (msg.compare(0, 8, "SHUTDOWN") == 0) {
 							done = true;
 							break;
 						}
-						std::cout << TXT_FAT << "Client " << i << ": " << TXT_NUL << msg << std::endl;
-						send(this->_connections[i], (msg + "\n").c_str(), msg.length() + 1, 0);
+						ft_sendmsg(this->_connections[i], msg);
 					}
 				}
 			}
