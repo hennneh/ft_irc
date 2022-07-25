e7bcd048c5efe8a292296a88d49f3d93d52336c8
Hendrik Lehmann <hendrik.lehmann96@gmail.com>
2022-07-25 08:44:42 +0200

    checking inputs strlen

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 
+int ft_strlen(char *s)
+{
+	int i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
 int	main(int argc, char **argv)
 {
 	(void)argv;
 	try
 	{
-		if (argc != 3)
+		if (argc != 3 || ft_strlen(argv[1]) == 0 || ft_strlen(argv[2]) == 0)
 			throw std::runtime_error("Please execute the binary as follows: ./ircserv <port> <password>");
 	}
 	catch (const std::exception &exception)
