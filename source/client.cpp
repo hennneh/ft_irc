6e50e9797ae6a5d0fa0d3fd62fbbad8b525d7474
Peter Stengl <pstengl@student.42wolfsburg.de>
2022-07-26 11:23:17 +0200

    Add basic member functions to client

---
+++ source/client.cpp
@@ -0,0 +1,72 @@
+#include "client.hpp"
+#include <string>
+
+//Constructors
+ft::Client::Client() {};
+ft::Client::Client(const int& socket, const std::string& nick, const std::string& user, const std::string& full)
+{
+	this->_socket = socket;
+	this->_nickname = nick;
+	this->_username = user;
+	this->_fullname = full;
+}
+
+ft::Client::Client(const Client& client)
+{
+	*this = client;
+}
+
+//Assignment Operator
+ft::Client& ft::Client::operator=(const ft::Client& client)
+{
+	this->_socket = client.getSocket();
+	this->_fullname = client.getFull();
+	this->_nickname = client.getNick();
+	this->_username = client.getUser();
+	return *this;
+}
+
+//Destructor
+ft::Client::~Client() {};
+
+//Getters
+std::string	ft::Client::getNick(void) const
+{
+	return this->_nickname;
+}
+
+std::string	ft::Client::getUser(void) const
+{
+	return this->_username;
+}
+
+std::string	ft::Client::getFull(void) const
+{
+	return this->_fullname;
+}
+
+int	ft::Client::getSocket(void) const
+{
+	return this->_socket;
+}
+
+//Setters
+void	ft::Client::setNick(const std::string& nick)
+{
+	this->_nickname = nick;
+}
+
+void	ft::Client::setUser(const std::string& user)
+{
+	this->_username = user;
+}
+
+void	ft::Client::setFull(const std::string& full)
+{
+	this->_fullname = full;
+}
+
+void	ft::Client::setSocket(const int& socket)
+{
+	this->_socket = socket;
+}
--- source/client.hpp
+++ source/client.hpp
@@ -3,22 +3,31 @@
 
 #include "irc.hpp"
 
-class Client
-{
-	public:
-		Client();
-		~Client();
-		std::string getNick(void);
-		std::string getUser(void);
-		std::string getFull(void);
-		void setNick(std::string nick);
-		void setUser(std::string user);
-		void setFull(std::string full);
+namespace ft {
+	class Client
+	{
+		private:
+			int			_socket;
+			std::string _nickname;
+			std::string _username;
+			std::string _fullname;
 
-	private:
-		std::string _nickname;
-		std::string _username;
-		std::string _fullname;
-};
+		public:
+			Client();
+			Client(const int& socket, const std::string& nick, const std::string& user, const std::string& full);
+			Client(const Client& client);
+			Client& operator=(const Client& client);
+			~Client();
+
+			std::string getNick(void) const;
+			std::string getUser(void) const;
+			std::string getFull(void) const;
+			int getSocket(void) const;
+			void setNick(const std::string& nick);
+			void setUser(const std::string& user);
+			void setFull(const std::string& full);
+			void setSocket(const int& socket);
+	};
+}
 
 #endif
