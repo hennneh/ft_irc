59e55fc9a8d50eeb13f08ed187c1bc0de63d95e2
Vincent Heymans <belgishboy@gmail.com>
2022-07-26 17:11:08 +0200

    Fixed #11. Message object working

---
+++ source/parse/message.hpp
@@ -0,0 +1,34 @@
+#ifndef MESSAGE_HPP
+#define MESSAGE_HPP
+
+#include "../irc.hpp"
+
+class Message
+{
+	public:
+
+		Message (std::string s)
+		{
+			size_t x = 0;
+			if (s[0] == ':')
+			{
+				x = s.find(' ');
+				this->prefix = s.substr(1, x - 1);
+				s.erase(0, x + 1);
+			}
+			x = s.find(' ');
+			this->command = s.substr(0, x);
+			s.erase(0, x + 1);
+			while (x != std::string::npos)
+			{
+				x = s.find(' ');
+				this->parameters.push_back(s.substr(0, x));
+				s.erase(0, x + 1);
+			}
+		}
+		std::string					prefix;
+		std::string					command;
+		std::vector<std::string>	parameters;
+};
+
+#endif
