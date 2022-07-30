9264bea8b6a798dad49f3ec18f3d6c389eec1f87
Vincent Heymans <belgishboy@gmail.com>
2022-07-30 15:44:36 +0200

    Update source/commands/nick.cpp
    Co-authored-by: GQDeltex <operitum@gmail.com>

--- source/commands/nick.cpp
+++ source/commands/nick.cpp
@@ -32,9 +32,9 @@ void cmd::nick(const ft::Message& msg, ft::Client& client, ft::IRC& irc)
 		return ;
 	}
 	client.setNick(msg.parameters.at(0));
-	client._pi = true;
 	if (client.getUser().empty())
 		return;
+	client._pi = true;
 	for(ft::IRC::connection_map::iterator it = irc._connections.begin(); it != irc._connections.end(); it++) {
 		if (it->second.getSocket() == client.getSocket()) {
 			cmd::welcome(msg, client, irc);
