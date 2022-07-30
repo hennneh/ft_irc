112bec848679d90311500e1e9cb0313c5da55729
Vincent Heymans <belgishboy@gmail.com>
2022-07-30 20:23:14 +0200

    sleep

--- source/commands/commands.hpp
+++ source/commands/commands.hpp
@@ -8,8 +8,21 @@
 namespace ft {
 	class IRC;
 }
+namespace m_channel {
+	void op_priv(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args);
+	void prvt(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args);
+	void scrt(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args);
+	void invt(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args);
+	void topic(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args);
+	void clsd(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args);
+	void ban_msk(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args);
+	void speak(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args);
+}
 
 namespace cmd {
+	typedef void (*m_channel_ft)(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args);
+	typedef std::map<char, m_channel_ft>		m_channel_map;
+
 	void user(const ft::Message& msg, ft::Client& client, ft::IRC& irc);
 	void motd(const ft::Message& msg, ft::Client& client, ft::IRC& irc);
 	void nick(const ft::Message& msg, ft::Client& client, ft::IRC& irc);
@@ -20,6 +33,24 @@ namespace cmd {
 	void pass(const ft::Message& msg, ft::Client& client, ft::IRC& irc);
 	void mode(const ft::Message& msg, ft::Client& client, ft::IRC& irc);
 	void modeUsr(const ft::Message& msg, ft::Client& client, ft::IRC& irc);
+
+	m_channel_map	_c_ft;
+	
+	void reg_ft(const char opt, m_channel_ft f)
+	{
+		_c_ft.insert(std::make_pair(opt, f));
+	}
+	void mk_map(void)
+	{
+		reg_ft('o', m_channel::op_priv);
+		reg_ft('p', m_channel::prvt);
+		reg_ft('s', m_channel::scrt);
+		reg_ft('i', m_channel::invt);
+		reg_ft('t', m_channel::topic);
+		reg_ft('n', m_channel::clsd);
+		reg_ft('b', m_channel::ban_msk);
+		reg_ft('v', m_channel::speak);
+	}
 }
 
 #endif
--- source/commands/mode.cpp
+++ source/commands/mode.cpp
@@ -17,15 +17,91 @@ l - set the user limit to channel;
 k - set a channel key (password).
 */
 
+void m_channel::op_priv(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args)
+{
+	(void)irc;
+	client.sendmsg(ft::Message(":Option op_priv:"));
+	(void)channel;
+	(void)sign;
+	(void)args;
+	return ;
+}
+void m_channel::prvt(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args)
+{
+	(void)irc;
+	client.sendmsg(ft::Message(":Option prvt:"));
+	(void)channel;
+	(void)sign;
+	(void)args;
+	return ;
+}
+void m_channel::scrt(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args)
+{
+	(void)irc;
+	client.sendmsg(ft::Message(":Option scrt:"));
+	(void)channel;
+	(void)sign;
+	(void)args;
+	return ;
+}
+void m_channel::invt(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args)
+{
+	(void)irc;
+	client.sendmsg(ft::Message(":Option invt:"));
+	(void)channel;
+	(void)sign;
+	(void)args;
+	return ;
+}
+void m_channel::topic(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args)
+{
+	(void)irc;
+	client.sendmsg(ft::Message(":Option topic:"));
+	(void)channel;
+	(void)sign;
+	(void)args;
+	return ;
+}
+void m_channel::clsd(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args)
+{
+	(void)irc;
+	client.sendmsg(ft::Message(":Option clsd:"));
+	(void)channel;
+	(void)sign;
+	(void)args;
+	return ;
+}
+void m_channel::ban_msk(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args)
+{
+	(void)irc;
+	client.sendmsg(ft::Message(":Option ban_msk:"));
+	(void)channel;
+	(void)sign;
+	(void)args;
+	return ;
+}
+void m_channel::speak(ft::Client& client, ft::IRC& irc, const std::string channel, bool sign, std::vector<std::string> args)
+{
+	(void)irc;
+	client.sendmsg(ft::Message(":Option speak:"));
+	(void)channel;
+	(void)sign;
+	(void)args;
+	return ;
+}
+
 
 void modeChannel(const ft::Message& msg, ft::Client& client, ft::IRC& irc)
 {
 	bool sign = false;
-	if (irc._connections.find(msg.parameters.at(0)) != irc._connections.end())
+	std::string _client = msg.parameters.at(0).substr(1, msg.parameters.at(0).length() - 1);
+	if (irc._connections.find(_client) != irc._connections.end()) // change to search channel names
 	{
-		client.sendmsg(ft::Message(":127.0.0.1 433 :Nickname is already in use")); //ERR_NICKNAMEINUSE
+		client.sendErrMsg(ERR_NOSUCHCHANNEL);
 		return ;
 	}
+	//check usr/channel permissions
+	
 }
 
 void mode(const ft::Message& msg, ft::Client& client, ft::IRC& irc)
@@ -46,12 +122,12 @@ ERR_NEEDMOREPARAMS		DONE
 RPL_CHANNELMODEIS
 ERR_CHANOPRIVSNEEDED
 ERR_NOSUCHNICK			USR
-ERR_NOTONCHANNEL
+ERR_NOTONCHANNEL		USR
 ERR_KEYSET
 RPL_BANLIST
 RPL_ENDOFBANLIST
 ERR_UNKNOWNMODE
-ERR_NOSUCHCHANNEL
+ERR_NOSUCHCHANNEL		DONE
 ERR_USERSDONTMATCH
 RPL_UMODEIS
 ERR_UMODEUNKNOWNFLAG
