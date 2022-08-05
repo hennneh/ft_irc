51ecf3dcd1478364b3452c7f8e526773546b55a2
Peter Stengl <pstengl@student.42wolfsburg.de>
2022-08-05 17:45:56 +0200

    OPER most probably works on the nick and not the user (like all other commands)

--- source/commands/connection/oper.cpp
+++ source/commands/connection/oper.cpp
@@ -8,22 +8,11 @@ void cmd::oper(const ft::Message & msg, ft::Client & client, ft::IRC & irc)
 		client.sendErrMsg(irc._hostname, ERR_NEEDMOREPARAMS, msg.command);
 		return ;
 	}
-	ft::IRC::connection_map::iterator i_clnt;
-	for (i_clnt = irc._connections.begin(); i_clnt != irc._connections.end(); i_clnt ++)
-	{
-		if (msg.parameters.at(0) == i_clnt->second.getUser())
-			break ;
-		if (i_clnt == --irc._connections.end())
-		{
-			client.sendErrMsg(irc._hostname, ERR_NOOPERHOST);
-			return ;
-		}
-	}
 	if (irc._op_password.empty() == false && msg.parameters.at(1) != irc._op_password)
 	{
 		client.sendErrMsg(irc._hostname, ERR_PASSWDMISMATCH, msg.command);
 		return ;
 	}
-	i_clnt->second._operator = true;
-	i_clnt->second.sendErrMsg(irc._hostname, RPL_YOUREOPER);
+	client._operator = true;
+	client.sendErrMsg(irc._hostname, RPL_YOUREOPER);
 }
