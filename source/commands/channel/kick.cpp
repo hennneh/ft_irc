b3b8b77f05f0be8dec7c9b18f6339a85645408be
Hendrik Lehmann <hendrik.lehmann96@gmail.com>
2022-08-10 17:21:01 +0200

    restructuring and pushing non-working version

--- source/commands/channel/kick.cpp
+++ source/commands/channel/kick.cpp
@@ -1,7 +1,15 @@
 #include "../commands.hpp"
 #include "../../irc.hpp"
 
-
+static void kick2(ft::Client & client, ft::Client & target, ft::Channel & channel)
+{
+	std::vector<std::string> arg;
+	arg.push_back(channel._name);
+	arg.push_back(target.getNick());
+	channel.sendMsg(ft::Message(client.getFullId(), "KICK", arg));
+	client.sendMsg(ft::Message(client.getFullId(), "KICK", arg));
+	channel._clients.erase(target.getNick());
+}
 void cmd::kick(const ft::Message& msg, ft::Client& client, ft::IRC& irc)
 {
 	if (msg.parameters.size() < 2 || msg.parameters.size() > 3)
@@ -11,7 +19,7 @@ void cmd::kick(const ft::Message& msg, ft::Client& client, ft::IRC& irc)
 	}
 	std::vector<std::string> _v_chnl = ft::split(msg.parameters.at(0), ',');
 	std::vector<std::string> _v_usr = ft::split(msg.parameters.at(1), ',');
-	if (_v_chnl.size() != _v_usr.size())
+	if (_v_chnl.size() != _v_usr.size() && _v_chnl.size() != 1)
 	{
 		client.sendErrMsg(irc._hostname, ERR_NEEDMOREPARAMS, msg.command);
 		return;
@@ -19,7 +27,7 @@ void cmd::kick(const ft::Message& msg, ft::Client& client, ft::IRC& irc)
 	std::string command = "No reason given";
 	if (msg.parameters.size() == 3)
 		command = msg.parameters.at(2);
-	for (size_t x = 0; x != _v_chnl.size(); x ++)
+	for (size_t x = 0; x != _v_chnl.size(); x++)
 	{
 		ft::IRC::_channel_map::iterator i_chnl = irc._channels.find(_v_chnl[x]);
 		if (i_chnl == irc._channels.end())
@@ -27,28 +35,38 @@ void cmd::kick(const ft::Message& msg, ft::Client& client, ft::IRC& irc)
 			client.sendErrMsg(irc._hostname, ERR_NOSUCHCHANNEL, _v_chnl[x]);
 			return;
 		}
-		ft::Channel _chnl = i_chnl->second;
-		ft::Channel::clients_map::iterator i_clnt = _chnl._clients.find(client.getNick());
-		if (i_clnt == _chnl._clients.end())
+		ft::Channel & _chnl = i_chnl->second;
+		ft::Channel::clients_map::iterator me = _chnl._clients.find(client.getNick());
+		if (me == _chnl._clients.end())
 		{
-			client.sendErrMsg(irc._hostname, ERR_NOTONCHANNEL, _v_chnl[x]);
-			return;
+			client.sendErrMsg(irc._hostname, ERR_NOTONCHANNEL, _chnl._name);
+			return ;
 		}
-		ft::ChannelUser _clnt = i_clnt->second;
-		if (_clnt.op_priv == false)
+		if (me->second.op_priv == false)
 		{
-			client.sendErrMsg(irc._hostname, ERR_CHANOPRIVSNEEDED, _v_chnl[x]);
-			return;
+			client.sendErrMsg(irc._hostname, ERR_CHANOPRIVSNEEDED, _chnl._name);
+			return ;
 		}
-		i_clnt = _chnl._clients.find(_v_usr[x]);
-		if (i_clnt == _chnl._clients.end())
+		if (_v_chnl.size() == _v_usr.size())
 		{
-			client.sendErrMsg(irc._hostname, ERR_NOTONCHANNEL, _v_chnl[x]);
-			return;
+			ft::Channel::clients_map::iterator i_clnt = _chnl._clients.find(_v_usr[x]);
+			if (i_clnt == _chnl._clients.end())
+			{
+				client.sendErrMsg(irc._hostname, ERR_USERNOTINCHANNEL, _v_usr.at(x), _v_chnl[x]);
+				return;
+			}
+			kick2(client, i_clnt->second.client, i_chnl->second);
+			return ;
 		}
-		ft::ChannelUser _usr = i_clnt->second;
-		_usr.client.sendMsg(ft::Message("You have been kicked for :" + command));
-		_usr.client.sendMsg(ft::Message(client.getNick(), "PART", _chnl.getName()));
-		_chnl.sendMsg(ft::Message(client.getNick(), "NOTICE", _usr.client.getNick() + " Kicked for:" + command)); // NOT SURE ABOUT THIS
+		for (unsigned int i = 0; i < _v_usr.size(); i++)
+		{
+			ft::Channel::clients_map::iterator i_clnt = _chnl._clients.find(_v_usr[x]);
+			if (i_clnt == _chnl._clients.end())
+			{
+				client.sendErrMsg(irc._hostname, ERR_USERNOTINCHANNEL, _v_usr.at(x), _v_chnl[x]);
+				return;
+			}
+			kick2(client, i_clnt->second.client, i_chnl->second);
+		}
 	}
 }
